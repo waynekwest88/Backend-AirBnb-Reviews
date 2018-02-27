59befd29db162e768c4c3849f4ab047302378ac3
SeSmC06 <dingpeng06@gmail.com>
2018-02-27 13:52:08 -0800

    webpack

---
+++ client/src/components/App.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+// import Message from './Message.jsx';
+// import Score from './Score.jsx';
+import axios from 'axios';
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      reviews: [],
+    }
+  }
+
+  getSingleMessage() {
+    axios.get('/reviews')
+      .then(data => console.log(`fetching from server ==> ${data}`))
+      .catch(error => console.log(`4000000044 with ${error}`))
+  }
+  
+  componentWillMount() {
+    this.getSingleMessage();
+  }
+
+  render() {
+    // const fakeShip = faked.ship;
+    return (
+      <div style={{textAlign: 'center'}}>
+        <h1>Fake Review</h1>
+        </div>
+    )
+  }
+}
---
+++ client/src/components/Message.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const Message = ({message}) => {
+  return (
+    <h5>nice place</h5>
+  )
+}
+
+export default Message;
