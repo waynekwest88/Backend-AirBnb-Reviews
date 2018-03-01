529145e9bac645f95a014f70dc3f987113e4cc52
SeSmC06 <dingpeng06@gmail.com>
2018-03-01 14:42:36 -0800

    refractered with styled components

--- client/src/components/Message.jsx
+++ client/src/components/Message.jsx
@@ -1,32 +1,52 @@
-import React from "react";
+import React from 'react';
+import styled from 'styled-components';
+
+const MessageWrapper = styled.div`
+  color: #484848;
+  font-family: Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif;
+  line-height: 22px;
+  font-weight: 300;
+  font-size: 17px;
+  display: block;
+`;
+
+const ImageWrapper = styled.img`
+  width: 48px;
+  height: 48px;
+  border-radius: 50%;
+`;
 
 const Message = ({ message, date, name, avatar }) => {
-  const style = {
-    divStyle: {
-      color: "#484848",
-      fontFamily:
-        "Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif",
-      lineHeight: "22px",
-      fontWeight: 300,
-      fontSize: "17px",
-      display: "block"
-    },
-    imgStyle: {
-      width: '48px',
-      height: '48px',
-      'borderRadius': '50%'
-    }
-  };
+
 
   return (
-    <div style={style.divStyle}>
-      <img src={avatar} style={style.imgStyle}/>
+    <MessageWrapper>
+      <ImageWrapper src={avatar} />
       <span>{name}</span>
       <span>{date}</span>
       <span>{date}</span>
       <span>{message}</span>
-    </div>
+    </MessageWrapper>
   );
 };
 
 export default Message;
+
+
+
+// const style = {
+//   divStyle: {
+//     color: '#484848',
+//     fontFamily:
+//       'Circular,-apple-system,BlinkMacSystemFont,Roboto,Helvetica Neue,sans-serif',
+//     lineHeight: '22px',
+//     fontWeight: 300,
+//     fontSize: '17px',
+//     display: 'block'
+//   },
+//   imgStyle: {
+//     width: '48px',
+//     height: '48px',
+//     borderRadius: '50%'
+//   }
+// };
