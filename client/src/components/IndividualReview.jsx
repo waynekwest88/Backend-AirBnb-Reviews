cfd164de37a9f27409058809e45eddbf36e71dc8
SeSmC06 <dingpeng06@gmail.com>
2018-03-05 15:27:24 -0800

    pagination finish

--- client/src/components/IndividualReview.jsx
+++ client/src/components/IndividualReview.jsx
@@ -2,24 +2,26 @@ import React from 'react';
 
 const IndividualReview = props => (
   <div className="container review">
-    <div className="container user" >
+    <div className="container user">
       <div className="row">
         <div className="col-s-2">
-          <img className="profilePhoto" src={props.review.userPhoto} alt="user" />
+          <img
+            className="profilePhoto"
+            src={props.review.image}
+            alt="user"
+          />
         </div>
         <div className="col">
           <div className="row">
-            <span className="username" >{props.review.userName}</span>
+            <span className="username">{props.review.guest_name}</span>
           </div>
           <div className="row">
-            <span className="reviewDate" >{props.review.date}</span>
+            <span className="reviewDate">{props.review.date}</span>
           </div>
         </div>
       </div>
     </div>
-    <div className="text">
-      {props.review.text}
-    </div>
+    <div className="text">{props.review.message}</div>
   </div>
 );
 
--- client/src/components/ReviewList.jsx
+++ client/src/components/ReviewList.jsx
@@ -1,74 +1,45 @@
 import React from 'react';
 import Message from './Message';
+import Navigation from './Navigation';
+import IndividualReview from './IndividualReview';
 
 export default class ReviewList extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      pager: {},
       currentPage: 1,
-      messages: this.props.reviews
+      reviews: this.props.reviews,
+      displayedReviews: this.props.reviews.slice(0, 7)
     };
+    this.updateCurrentPage = this.updateCurrentPage.bind(this);
   }
 
-  componentWillMount() {
-    this.setupData(this.props.reviews, message =>
-      this.setState({ messages: message })
-    );
+  updateCurrentPage(newPage) {
+    this.setState({ currentPage: newPage }, () => {
+      this.updateDisplayedReview();
+    });
   }
 
-  setupData(msgObj, cb) {
-    let total = [];
-    let inner = [];
-    if (msgObj.length < 7) {
-      return msgObj;
-    }
-    for (let i = 0; i < msgObj.length; i++) {
-      if (inner.length === 7) {
-        total.push(inner);
-        inner = [];
-      }
-      inner.push(msgObj[i]);
-    }
-    cb(total);
-  }
-
-  handlePageChange(page) {
-    this.setStage({ page: page });
-  }
-
-  setPage(page) {
-    // TODO: every page gets 7 items, and render out buttons based on total message
-  }
-
-  getPage() {
-    // TODO: retrieve the stored message obj from slicing
+  updateDisplayedReview() {
+    const startIndex = (this.state.currentPage - 1) * 7;
+    const endIndex = startIndex + 7;
+    this.setState({
+      displayedReviews: this.state.reviews.slice(startIndex, endIndex)
+    });
   }
 
   render() {
     return (
-      <div>
-        <Message msgObj={this.state.messages[0]} />
+      <div id="review">
+        {this.state.displayedReviews.map(review => (
+          <IndividualReview review={review} key={review.id} />
+      ))}
+        <Navigation
+          pages={Math.ceil(this.state.reviews.length / 7)}
+          clickHandler={this.updateCurrentPage}
+        />
       </div>
+
     );
   }
-}
-// setPage(page) {
-//   var items = this.props.items;
-//   var pager = this.state.pager;
-//   if (page < 1 || page > pager.totalPages) {
-//       return;
-//   }
-//   // get new pager object for specified page
-//   pager = this.getPager(items.length, page);
-//   // get new page of items from items array
-//   var pageOfItems = items.slice(pager.startIndex, pager.endIndex + 1);
-//   // update state
-//   this.setState({ pager: pager });
-//   // call change page function in parent component
-//   this.props.onChangePage(pageOfItems);
-// }
-
-{
-  /* <Message msgObj={this.state.messages[0]} /> */
 }
