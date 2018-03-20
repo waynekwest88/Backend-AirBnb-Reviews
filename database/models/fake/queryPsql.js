0d2bc6c710417ac06a6667c25c83b403cc0b1884
waynekwest88@gmail.com <waynekwest88@gmail.com>
2018-03-20 16:51:42 -0700

    got postgreSQL queries at around 25 ms

--- database/models/fake/queryPsql.js
+++ database/models/fake/queryPsql.js
@@ -1,12 +1,10 @@
-const pgp = require('pg-promise')({
-  capSQL: true // generate capitalized SQL 
-});
+const pgp = require('pg-promise')();
 
 const db = pgp('postgres://localhost:5432/reviews');
 
 let query = () => {
     let startTime = new Date().getTime();
-    db.any('SELECT * FROM list WHERE id = 9999998')
+    db.any('SELECT * FROM userpage WHERE id = 9999998')
     .then((data) => {
         let endTime = new Date().getTime();
         console.log(data);
